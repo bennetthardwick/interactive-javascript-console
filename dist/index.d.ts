f623e2469c0db12aae9034db66db780c0bace7d9
Bennett Hardwick <bennetthardwick@icloud.com>
2018-09-03 22:04:58 +1000

    Updated: build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,17 @@
-export {};
+import "../node_modules/codemirror/mode/javascript/javascript";
+import "./style.scss";
+export declare class InteractiveConsole {
+    private readonly textarea;
+    private readonly execute;
+    private readonly reset;
+    private readonly output;
+    private readonly document;
+    private editor;
+    private frame;
+    constructor(textarea: HTMLTextAreaElement, execute: HTMLButtonElement, reset: HTMLButtonElement, output: HTMLDivElement, document: Document);
+    run(): void;
+    private log;
+    private error;
+    private resultToConsole;
+    private alert;
+}
